9c49e4febaa18952308c37be919006ebf782cfe6
Abshir <abshirm@live.com>
2019-07-11 10:55:45 -0700

    initial commit

---
+++ Voice Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice Assistant
+//
+//  Created by Abshir Mohamed on 7/11/19.
+//  Copyright © 2019 Abshir Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
